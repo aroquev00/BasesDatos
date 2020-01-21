eee1276fbc7e2c33033c367208ed59a0c084c756
aroquev00 <aroquev00@users.noreply.github.com>
2020-01-20 21:54:17 -0600

    Make changes to company queries

--- queriesCompany.sql
+++ queriesCompany.sql
@@ -1,3 +1,5 @@
+-- #11 Armando Roque Villasana A01138717
+
 -- 1. List Department names
 SELECT dname 
 FROM department;
@@ -39,21 +41,52 @@ ON mgrssn=ssn
 WHERE plocation='Stafford';
 
 --7. Make a list of project numbers for projects that involve an employee whose last name is 'Smith', either as a worker or as a manager of the department that controls the project.
-SELECT dnumber
-FROM department 
-WHERE dnumber = (
-    SELECT dno 
+SELECT pno
+FROM works_on 
+WHERE essn = (
+    SELECT ssn 
     FROM employee
     WHERE lname='Smith'
+)
+UNION
+SELECT pnumber
+FROM project
+WHERE dnum = (
+    SELECT dnumber
+    FROM department
+    WHERE mgrssn = (
+        SELECT ssn
+        FROM employee
+        WHERE lname = 'Smith'
+    )
 );
+
+-- with joins
+SELECT pno
+FROM employee JOIN works_on
+ON ssn=essn
+WHERE lname='Smith'
+UNION
+SELECT lname
+FROM project JOIN department
+ON dnum=dnumber
+JOIN employee
+ON mgrssn=ssn
+WHERE lname='Smith';
 
 --8. Retrieve the names of employees who have no dependents.
 SELECT fname, lname
 FROM employee
-WHERE ssn NOT iN (
+WHERE ssn NOT IN (
     SELECT essn
     FROM dependent
 );
+
+SELECT fname, lname
+FROM employee LEFT JOIN dependent
+ON ssn=essn
+WHERE essn IS NULL;
+
 
 --9. Retrieve the names of managers who have at least one dependent.
 SELECT fname, lname
@@ -84,7 +117,7 @@ WHERE 2 <= (
 );
 
 --12. Find the names of employees who work on all the projects controlled by department number 5.
-SELECT DISTINCT fname, lname
+SELECT fname, lname
 FROM employee
 WHERE (
     SELECT COUNT(dnum)
