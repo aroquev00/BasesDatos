c3294cb67fc2449fd9ee5692be88384be0fb2f70
aroquev00 <aroquev00@users.noreply.github.com>
2020-01-25 13:50:31 -0600

    Work on queries Stay At Home

--- queriesStayAtHome.sql
+++ queriesStayAtHome.sql
@@ -18,15 +18,15 @@ FROM Video V
 JOIN Director D ON V.DirectorNo = D.DirectorNo;
 
 -- 5. What is the Inventory value that StayHome has?  (Each copy of the movie is valued at the Movie Price)
-SELECT SUM(V.price)
+SELECT SUM(V.price) AS InventoryValue
 FROM VideoForRent VFR 
 JOIN Video V ON VFR.catalogNo = V.catalogNo
 
 -- 6. Get the video title and the number of copies for each movie
-SELECT V.title, COUNT(*) AS Copies
-FROM VideoForRent VFR 
-JOIN Video V ON VFR.catalogNo = V.catalogNo
-GROUP BY V.catalogNo;
+SELECT V.title, COUNT(VFR.catalogNo) AS Copies
+FROM Video V
+JOIN VideoForRent VFR ON V.catalogNo = VFR.catalogNo
+GROUP BY V.title;
 
 -- 7. Get the category  and the number of movies in each category
 SELECT V.category, COUNT(*) AS NumTitles
@@ -34,45 +34,72 @@ FROM Video V
 GROUP BY category;
  
 -- 8. Get the movie titles that have the highest daily rent
-SELECT V.title
+SELECT V.title, dailyRental
 FROM Video V
 ORDER BY dailyRental DESC;
 
 -- 9. Get the title of the movie and the income obtained by rentals
-SELECT V.title, V.price * COUNT() AS income
+SELECT V.title, SUM(V.dailyRental * DATEDIFF(DAY, dateOut, dateReturn))
 FROM RentalAgreement RA
 JOIN VideoForRent VFR ON RA.videoNo = VFR.videoNo
 JOIN Video V ON VFR.catalogNo = V.catalogNo
-GROUP BY catalogNo
-ORDER BY income;
+GROUP BY V.title;
 
 -- 10. Get first name, last name of members that have a movie rented and still  not returned
 SELECT DISTINCT fName, lName 
 FROM RentalAgreement RA
 JOIN Member M ON RA.memberNo = M.memberNo
-WHERE DATEDIFF(DAY, dateReturn, dateOut) > DATEDIFF(DAY, GETDATE(), dateOut);
+WHERE DATEDIFF(DAY, dateOut, dateReturn) > DATEDIFF(DAY, dateOut, GETDATE());
 
--- 11. Get Title of the movie with the highest  number of rentals
-SELECT V.title, COUNT(*) AS cuenta
+-- or if is null
+SELECT DISTINCT fName, lName 
+FROM RentalAgreement RA
+JOIN Member M ON RA.memberNo = M.memberNo
+WHERE dateReturn IS NULL;
+
+-- 11. Get Title of the movie with the highest number of rentals
+SELECT V.title, COUNT(V.title) AS cuenta
 FROM RentalAgreement RA
 JOIN VideoForRent VFR ON RA.videoNo = VFR.videoNo
 JOIN Video V ON VFR.catalogNo = V.catalogNo
-GROUP BY catalogNo
-ORDER BY cuenta;
+GROUP BY V.title
+HAVING COUNT(V.title) >= ALL (
+	SELECT COUNT(V.title) AS cuenta
+	FROM RentalAgreement RA
+	JOIN VideoForRent VFR ON RA.videoNo = VFR.videoNo
+	JOIN Video V ON VFR.catalogNo = V.catalogNo
+	GROUP BY V.title
+);
+
+SELECT TOP 1 V.title, COUNT(V.title) AS cuenta
+FROM RentalAgreement RA
+JOIN VideoForRent VFR ON RA.videoNo = VFR.videoNo
+JOIN Video V ON VFR.catalogNo = V.catalogNo
+GROUP BY V.title
+ORDER BY cuenta DESC;
 
 -- 12. Get the first name, last name and the birthday of the members of the club who celebrate   birthday today
 SELECT fName, lName, DOB
 FROM Member
 WHERE DATEDIFF(DAY, GETDATE(), DOB) = 0;
 
+-- or
+SELECT fName, lName, DOB
+FROM Member
+WHERE DAY(DOB) = DAY(GETDATE()) AND MONTH(DOB) = MONTH(GETDATE());
+
 -- 13. Get Title of the movie  that has not been rented ever
 SELECT V.title
 FROM Video V
 WHERE V.catalogNo NOT IN (
-    SELECT DISTINCT RA.catalogNo
+    SELECT DISTINCT VFR.catalogNo
     FROM RentalAgreement RA
     JOIN VideoForRent VFR ON RA.videoNo = VFR.videoNo
 );
+
+SELECT *  FROM RentalAgreement RA
+    JOIN VideoForRent VFR ON RA.videoNo = VFR.videoNo
+	join video V ON VFR.catalogNo = V.catalogNo
 
 -- 14. Get the first name, last name of the members that were born the same year as Lorna Smith, but do not include Lorna Smith in the output
 SELECT fName, lName 
@@ -88,8 +115,8 @@ FROM Member
 WHERE fName = 'Lorna' AND lName = 'Smith';
 
 -- 15. Get the first name and last name (fName y lName) of the members that have rented the highest number of videos. 
-SELECT fName, lName, COUNT(*) AS cuenta
+SELECT TOP 3 fName, lName, COUNT(*) AS rentals
 FROM RentalAgreement RA
 JOIN Member M ON RA.memberNo = M.memberNo
-GROUP BY memberNo
-ORDER BY cuenta;
+GROUP BY fName, lName
+ORDER BY rentals DESC;
